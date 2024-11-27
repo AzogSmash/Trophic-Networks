e3442bed67a124568091d2a9174f6c74b952fb9c
Tristan1607 <148858769+Tristan1607@users.noreply.github.com>
2024-11-28 00:53:07 +0100

    fonction pour info du resau

--- main.c
+++ main.c
@@ -12,6 +12,22 @@ typedef struct {
     int destination;
     float ponderation;
 } Arc;
+// Fonction pour afficher les informations sur le reseau trophique
+void afficherReseau(Sommet *sommets, int nbSommets, Arc *arcs, int nbArcs, const char *nomEcosysteme, const char *climat) {
+    printf("\n--- Description du reseau trophique : %s ---\n", nomEcosysteme);
+    printf("Climat : %s\n", climat);
+    printf("Liste des sommets (especes) :\n");
+    for (int i = 0; i < nbSommets; i++) {
+        printf("  - %s\n", sommets[i].nom);
+    }
+    printf("\nListe des arcs (interactions) avec ponderations :\n");
+    for (int i = 0; i < nbArcs; i++) {
+        printf("  - %s -> %s : %.1f\n",
+               sommets[arcs[i].source].nom,
+               sommets[arcs[i].destination].nom,
+               arcs[i].ponderation);
+    }
+}
 
 
 int main() {
